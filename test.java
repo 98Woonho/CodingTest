b7a7fd78ca58c09fdb9714a6e66fbdb38c23d80f
98Woonho <9woonho8@gmail.com>
2024-08-12 16:00:33 +0900

    .

--- test.java
+++ test.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalInt;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println(1%0);
+        int[] a = {1,2,3,4,5};
+
+        List<Integer> list = new ArrayList<Integer>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+
+        String b = "abc";
     }
 }
