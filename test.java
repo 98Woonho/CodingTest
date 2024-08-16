2f400c332c025551ea971e0c3af5653b310ced0b
98Woonho <9woonho8@gmail.com>
2024-08-16 17:24:37 +0900

    .

--- test.java
+++ test.java
@@ -4,14 +4,8 @@ import static java.util.Arrays.asList;
 
 public class test {
     public static void main(String[] args) {
-        int[][] arr = {{1,2,3}};
+        String a = ".d90k23dwd#.";
 
-        int num = 1;
-
-        List<int[]> list1 = new ArrayList<>();
-
-        List<int[]> list = new ArrayList<>(Arrays.asList(arr));
-
-        list.sort(Comparator.comparingInt(a -> a[num]));
+        System.out.println(a.substring(0, a.length() - 1));
     }
 }
