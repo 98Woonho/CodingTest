89d84b44be73c9f1c57286b993d96659493a00eb
98Woonho <9woonho8@gmail.com>
2024-08-29 17:05:14 +0900

    .

--- test.java
+++ test.java
@@ -2,15 +2,12 @@ import java.sql.SQLOutput;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
 
 public class test {
     public static void main(String[] args) {
-        HashMap<Integer, Integer> hm = new HashMap<>();
-
-        hm.put(1, 2);
-
-        System.out.println(hm.containsKey(2));
+        int[] a = new int[]{1,2,3};
     }
 }
