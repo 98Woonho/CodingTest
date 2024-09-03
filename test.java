c6bad21acab46c88fc9e90d1e6cd41ed3227d7cd
98Woonho <9woonho8@gmail.com>
2024-09-03 16:16:17 +0900

    .

--- test.java
+++ test.java
@@ -1,10 +1,19 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        String a = "1 2 3 4";
-        String[] arr = a.split(" ");
+        int[] arr = {1,2,3,4,5};
 
+        List<Integer> list = new ArrayList<>();
 
+        list.add(1);
+        list.add(2);
+
+        list.sort(Comparator.naturalOrder());
+        list.sort(Comparator.reverseOrder());
+
+        Arrays.sort(arr);
+
+        System.out.println(list);
     }
 }
