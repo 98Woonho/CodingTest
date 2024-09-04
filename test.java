cab5b44af1e01799a9ae9fe55c25b4070853662d
98Woonho <9woonho8@gmail.com>
2024-09-04 16:40:20 +0900

    .

--- test.java
+++ test.java
@@ -2,18 +2,6 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
-
-        List<Integer> list = new ArrayList<>();
-
-        list.add(1);
-        list.add(2);
-
-        list.sort(Comparator.naturalOrder());
-        list.sort(Comparator.reverseOrder());
-
-        Arrays.sort(arr);
-
-        System.out.println(list);
+        Integer.toBi
     }
 }
