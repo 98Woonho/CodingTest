0217b0f9b426ba7041f9c4e345d8aed19c9b27ed
98Woonho <9woonho8@gmail.com>
2024-09-05 14:14:22 +0900

    .

--- test.java
+++ test.java
@@ -3,5 +3,21 @@ import java.util.*;
 public class test {
     public static void main(String[] args) {
 
+        System.out.println(fibonacci(100000));
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        int a = fibonacci(n - 1) % 1234567;
+        int b = fibonacci(n - 2) % 1234567;
+
+        return (a + b) % 1234567;
     }
 }
