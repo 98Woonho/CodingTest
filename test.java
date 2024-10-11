98103e29e59f033c585b451d96fe70b0f344d25a
98Woonho <9woonho8@gmail.com>
2024-10-11 18:07:00 +0900

    .

--- test.java
+++ test.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        int[][] arr = new int[10000000][10000000];
+        List<Integer> list = new ArrayList<>();
+
+        list.
 
 
     }
